d595ce23aeec37a89463c18f7e8ef53a78f1458e
Michaël Arnauts <michael.arnauts@gmail.com>
2019-07-08 21:26:20 +0200

    Fox tox

--- resources/lib/plugin.py
+++ resources/lib/plugin.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from __future__ import absolute_import, division, unicode_literals
+from __future__ import absolute_import, division, unicode_literals, print_function
 
 import logging
 
-import routing
+import routing  # noqa
 import xbmc
 import xbmcplugin
 from xbmcaddon import Addon
