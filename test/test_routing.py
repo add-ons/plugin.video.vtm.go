32f9751f4595fb056a8b9f1b939ef15eadecb416
Michaël Arnauts <michael.arnauts@destiny.be>
2019-09-17 18:00:29 +0200

    Import os

--- test/test_routing.py
+++ test/test_routing.py
@@ -4,6 +4,8 @@
 # pylint: disable=missing-docstring
 
 from __future__ import absolute_import, division, print_function, unicode_literals
+
+import os
 import unittest
 from resources.lib import plugin
 
