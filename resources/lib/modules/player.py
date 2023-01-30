8ec305e37b227ec0c0571fb6b6fd87a84e45ea03
mediaminister <mediaminister@gmail.com>
2023-01-30 14:05:02 +0100

    Update to Popcorn SDK 6

--- resources/lib/modules/player.py
+++ resources/lib/modules/player.py
@@ -150,10 +150,8 @@ class Player:
         else:
             url = resolved_stream.url
 
-        license_key = self._stream.create_license_key(resolved_stream.license_url)
-
         # Play this item
-        kodiutils.play(url, license_key, resolved_stream.title, {}, info_dict, prop_dict, stream_dict)
+        kodiutils.play(resolved_stream.url, resolved_stream.license_key, resolved_stream.title, {}, info_dict, prop_dict, stream_dict)
 
         # Wait for playback to start
         kodi_player = KodiPlayer()
